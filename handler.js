c1f3ed9006645ef18bad00f907c2b75383bdf7e4
AlbanaCelepija <acelepija@fbk.eu>
2020-04-07 11:21:27 +0200

    fix(handler)improvements regarding policy and async

--- handler.js
+++ handler.js
@@ -19,11 +19,11 @@ var TYPE_PROVENANCE          = "/provenance-data/process-groups/"; // view prove
 var TYPE_DATA                = "/data/process-groups/"; // view/empty queues, view metadata, submit replays
 var ACTION_READ              = "read";
 var ACTION_WRITE             = "write";
-var ADMIN_USER               = "CN=admin, OU=NIFI";
+var ADMIN_USER               = "admin";
 
 const httpsAgent = new https.Agent({
-  pfx: fs.readFileSync("./certificates/CN=admin_OU=NIFI.p12"),
-  passphrase: NIFI_CERTIFICATE_PASSW
+  pfx: fs.readFileSync("./certificates/certificate.p12"),
+  passphrase: NIFI_CERTIFICATE_PASSW, 
 })
 
 /**
